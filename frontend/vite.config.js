429b41857be29cd4eedacf359a2169f0db629e3a
Kanchan Mishra <133621440+Kanchan154@users.noreply.github.com>
2026-02-09 22:09:01 +0530

    Fix configuration syntax in vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,10 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import dotenv from 'dotenv'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   define: {
       'process.env.AUTH_API' : JSON.stringify(process.env.AUTH_API)
     },
-})
+});
